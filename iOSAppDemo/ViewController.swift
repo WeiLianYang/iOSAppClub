f96ae9f452b39c5ffc8a4fc5aeef5d0bb7d52f10
William <yangchen_0808@163.com>
2021-12-14 22:35:25 +0800

    add UITextField demo

--- iOSAppDemo/ViewController.swift
+++ iOSAppDemo/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +22,8 @@ class ViewController: UIViewController {
         addUIImageView1()
         
         addUIImageView2()
+        
+        addUITextField()
     }
     
     // 添加简单的 UILabel
@@ -29,9 +31,9 @@ class ViewController: UIViewController {
         // 创建 UILabel 控件
         let label = UILabel(frame: CGRect(x: 0, y: 30, width: 375, height: 130))
         // UILabel 设置文本
-        label.text = "自定义简单的便签控件：设置长文本，字体加粗，字号22，颜色为蓝色，阴影为橙色，位置偏移参数为2，文字对齐模式居左，显示行数为5行"
+        label.text = "自定义简单的便签控件：设置长文本，字体加粗，字号18，颜色为蓝色，阴影为橙色，位置偏移参数为2，文字对齐模式居左，显示行数为4行"
         // UILabel 设置字体，字号22，加粗
-        label.font = UIFont.boldSystemFont(ofSize: 22)
+        label.font = UIFont.boldSystemFont(ofSize: 18)
         // UILabel 设置字体颜色
         label.textColor = UIColor.blue
         // UILabel 设置阴影颜色
@@ -41,7 +43,7 @@ class ViewController: UIViewController {
         // UILabel 设置文字对齐模式，靠左对齐
         label.textAlignment = NSTextAlignment.left
         // UILabel 设置文本显示行数，为 0 表示不限制行数
-        label.numberOfLines = 5
+        label.numberOfLines = 4
         
         // 添加控件到视图上
         self.view.addSubview(label)
@@ -49,7 +51,7 @@ class ViewController: UIViewController {
     
     // 添加个性化的 UILabel
     func addSpecialUILabel() {
-        let label = UILabel(frame: CGRect(x: 15, y: 180, width: 375, height: 60))
+        let label = UILabel(frame: CGRect(x: 15, y: 160, width: 375, height: 60))
         // UILabel 设置默认字体，字号12，加粗
         label.font = UIFont.systemFont(ofSize: 14)
         label.numberOfLines = 0
@@ -89,7 +91,7 @@ class ViewController: UIViewController {
         
         let uiButton = UIButton(type: UIButton.ButtonType.system)
         // UIButton 设置位置
-        uiButton.frame = CGRect(x: 10, y: 280, width: 360, height: 100)
+        uiButton.frame = CGRect(x: 10, y: 240, width: 360, height: 40)
         // UIButton 设置颜色
         uiButton.backgroundColor = UIColor.cyan
         // UIButton 设置标题
@@ -103,7 +105,7 @@ class ViewController: UIViewController {
         self.view.addSubview(uiButton)
         
         let uiButton2 = UIButton(type: UIButton.ButtonType.custom)
-        uiButton2.frame = CGRect(x: 10, y: 400, width: 360, height: 100)
+        uiButton2.frame = CGRect(x: 10, y: 290, width: 360, height: 100)
         uiButton2.backgroundColor = UIColor.cyan
         uiButton2.setTitle("Content Image", for: .normal)
         uiButton2.setTitleColor(UIColor.black, for: .normal)
@@ -112,7 +114,7 @@ class ViewController: UIViewController {
         self.view.addSubview(uiButton2)
         
         let uiButton3 = UIButton(type: UIButton.ButtonType.custom)
-        uiButton3.frame = CGRect(x: 10, y: 520, width: 360, height: 100)
+        uiButton3.frame = CGRect(x: 10, y: 400, width: 360, height: 100)
         uiButton3.backgroundColor = UIColor.cyan
         uiButton3.setTitle("Background Image", for: .normal)
         uiButton3.setTitleColor(UIColor.black, for: .normal)
@@ -133,7 +135,7 @@ class ViewController: UIViewController {
         
         let imageView = UIImageView(image: uiImage)
         // 设置 UIImageView 的尺寸和位置
-        imageView.frame = CGRect(x: 30, y: 640, width: 100, height: 100)
+        imageView.frame = CGRect(x: 30, y: 510, width: 100, height: 100)
         self.view.addSubview(imageView)
     }
     
@@ -145,7 +147,7 @@ class ViewController: UIViewController {
             imageArray.append(uiImage!)
         }
        
-        let imageView = UIImageView(frame: CGRect(x: 150, y: 640, width: 100, height: 100))
+        let imageView = UIImageView(frame: CGRect(x: 150, y: 510, width: 100, height: 100))
         // 设置 UIImageView 的动画图片数组
         imageView.animationImages = imageArray
         // 设置 UIImageView 动画播放时长
@@ -156,6 +158,45 @@ class ViewController: UIViewController {
         
         imageView.startAnimating()
     }
+    
+    func addUITextField() {
+        // 创建 UITextField
+        let textField = UITextField(frame: CGRect(x: 30, y: 620, width: 300, height: 40))
+        // 设置 UITextField 的边框风格
+        textField.borderStyle = UITextField.BorderStyle.roundedRect
+        // 设置 UITextField 的文字颜色
+        textField.textColor = UIColor.black
+        // 设置 UITextField 的文字对齐方式
+        textField.textAlignment = NSTextAlignment.left
+        // 设置 UITextField 的提示文字
+        textField.placeholder = "please input your name"
+                
+        // 设置 UITextField 的左视图
+        let leftImageView = UIImageView(image: UIImage(named: "portrait"))
+        leftImageView.frame = CGRect(x: 0, y: 0, width: 35, height: 35)
+        textField.leftView = leftImageView
+        
+        // 设置 UITextField 的右视图
+        let rightImageView = UIImageView(image: UIImage(named: "EyeInvisible"))
+        rightImageView.frame = CGRect(x: 0, y: 0, width: 35, height: 35)
+        textField.rightView = rightImageView
+        
+        // 设置 UITextField 的左视图显示模式为一直显示
+        textField.leftViewMode = UITextField.ViewMode.always
+        // 设置 UITextField 的右视图显示模式为不处于编辑状态时才显示
+        textField.rightViewMode = UITextField.ViewMode.unlessEditing
+        
+        textField.clearButtonMode = UITextField.ViewMode.whileEditing
+        
+        // 设置 UITextField 的代理为当前视图控制器
+//        textField.delegate = self as UITextViewDelegate
+        
+        self.view.addSubview(textField)
+    }
+    
+//    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+//
+//    }
     
 }
 
