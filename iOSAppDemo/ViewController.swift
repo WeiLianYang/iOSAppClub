8c5be5b23d077e8bbd6e632742b392eca9414943
William <yangchen_0808@163.com>
2021-12-20 22:22:45 +0800

    add UISwitch / UIPageControl / UISegmentedControl demo

--- iOSAppDemo/ViewController.swift
+++ iOSAppDemo/ViewController.swift
@@ -24,12 +24,20 @@ class ViewController: UIViewController, UITextViewDelegate {
         addUIImageView2()
         
         addUITextField()
+        
+        addUISwitchView1()
+        addUISwitchView2()
+        addUISwitchView3()
+        
+        addUIPageControlView()
+        
+        addUISegmentedControlView()
     }
     
     // 添加简单的 UILabel
     func addNormalUILabel() {
         // 创建 UILabel 控件
-        let label = UILabel(frame: CGRect(x: 0, y: 30, width: 375, height: 130))
+        let label = UILabel(frame: CGRect(x: 10, y: 30, width: 375, height: 130))
         // UILabel 设置文本
         label.text = "自定义简单的便签控件：设置长文本，字体加粗，字号18，颜色为蓝色，阴影为橙色，位置偏移参数为2，文字对齐模式居左，显示行数为4行"
         // UILabel 设置字体，字号22，加粗
@@ -51,7 +59,7 @@ class ViewController: UIViewController, UITextViewDelegate {
     
     // 添加个性化的 UILabel
     func addSpecialUILabel() {
-        let label = UILabel(frame: CGRect(x: 15, y: 160, width: 375, height: 60))
+        let label = UILabel(frame: CGRect(x: 10, y: 150, width: 375, height: 60))
         // UILabel 设置默认字体，字号12，加粗
         label.font = UIFont.systemFont(ofSize: 14)
         label.numberOfLines = 0
@@ -91,7 +99,7 @@ class ViewController: UIViewController, UITextViewDelegate {
         
         let uiButton = UIButton(type: UIButton.ButtonType.system)
         // UIButton 设置位置
-        uiButton.frame = CGRect(x: 10, y: 240, width: 360, height: 40)
+        uiButton.frame = CGRect(x: 10, y: 230, width: 360, height: 38)
         // UIButton 设置颜色
         uiButton.backgroundColor = UIColor.cyan
         // UIButton 设置标题
@@ -105,7 +113,7 @@ class ViewController: UIViewController, UITextViewDelegate {
         self.view.addSubview(uiButton)
         
         let uiButton2 = UIButton(type: UIButton.ButtonType.custom)
-        uiButton2.frame = CGRect(x: 10, y: 290, width: 360, height: 100)
+        uiButton2.frame = CGRect(x: 10, y: 280, width: 360, height: 80)
         uiButton2.backgroundColor = UIColor.cyan
         uiButton2.setTitle("Content Image", for: .normal)
         uiButton2.setTitleColor(UIColor.black, for: .normal)
@@ -114,7 +122,7 @@ class ViewController: UIViewController, UITextViewDelegate {
         self.view.addSubview(uiButton2)
         
         let uiButton3 = UIButton(type: UIButton.ButtonType.custom)
-        uiButton3.frame = CGRect(x: 10, y: 400, width: 360, height: 100)
+        uiButton3.frame = CGRect(x: 10, y: 370, width: 360, height: 80)
         uiButton3.backgroundColor = UIColor.cyan
         uiButton3.setTitle("Background Image", for: .normal)
         uiButton3.setTitleColor(UIColor.black, for: .normal)
@@ -135,7 +143,7 @@ class ViewController: UIViewController, UITextViewDelegate {
         
         let imageView = UIImageView(image: uiImage)
         // 设置 UIImageView 的尺寸和位置
-        imageView.frame = CGRect(x: 30, y: 510, width: 100, height: 100)
+        imageView.frame = CGRect(x: 30, y: 460, width: 80, height: 80)
         self.view.addSubview(imageView)
     }
     
@@ -147,7 +155,7 @@ class ViewController: UIViewController, UITextViewDelegate {
             imageArray.append(uiImage!)
         }
        
-        let imageView = UIImageView(frame: CGRect(x: 150, y: 510, width: 100, height: 100))
+        let imageView = UIImageView(frame: CGRect(x: 150, y: 460, width: 80, height: 80))
         // 设置 UIImageView 的动画图片数组
         imageView.animationImages = imageArray
         // 设置 UIImageView 动画播放时长
@@ -161,7 +169,7 @@ class ViewController: UIViewController, UITextViewDelegate {
     
     func addUITextField() {
         // 创建 UITextField
-        let textField = UITextField(frame: CGRect(x: 30, y: 620, width: 300, height: 40))
+        let textField = UITextField(frame: CGRect(x: 30, y: 550, width: 300, height: 40))
         // 设置 UITextField 的边框风格
         textField.borderStyle = UITextField.BorderStyle.roundedRect
         // 设置 UITextField 的文字颜色
@@ -197,6 +205,166 @@ class ViewController: UIViewController, UITextViewDelegate {
 //    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
 //
 //    }
+    
+    func addUISwitchView1() {
+        // 创建开关控件
+        let si = UISwitch()
+        // 设置位置
+        si.center = CGPoint(x: 50, y: 620)
+        // 设置 UISwitch 开启状态的颜色
+        si.onTintColor = UIColor.green
+        // 设置 UISwitch 普通状态的边框颜色
+        si.tintColor = UIColor.red
+        // 设置 UISwitch 滑块的颜色
+        si.thumbTintColor = UIColor.white
+        // 设置 UISwitch 初始状态
+        si.isOn = true
+        // 设置 UISwitch 开关样式
+        si.preferredStyle = UISwitch.Style.automatic
+        
+        // 监听 UISwitch 开关状态变化
+        si.addTarget(self, action: #selector(change), for: UIControl.Event.valueChanged)
+        
+        self.view.addSubview(si)
+    }
+    
+    @objc func change(si: UISwitch) {
+        print("开关状态：\(si.isOn)")
+    }
+    
+    func addUISwitchView2() {
+        // 创建开关控件
+        let si = UISwitch()
+        // 设置位置
+        si.center = CGPoint(x: 120, y: 620)
+        // 设置 UISwitch 开启状态的颜色
+        si.onTintColor = UIColor.green
+        // 设置 UISwitch 普通状态的边框颜色
+        si.tintColor = UIColor.red
+        // 设置 UISwitch 滑块的颜色
+        si.thumbTintColor = UIColor.white
+        // 设置 UISwitch 初始状态
+        si.isOn = false
+        // 设置 UISwitch 开关样式
+        si.preferredStyle = UISwitch.Style.sliding
+        self.view.addSubview(si)
+    }
+    
+    func addUISwitchView3() {
+        // 创建开关控件
+        let si = UISwitch()
+        // 设置位置
+        si.center = CGPoint(x: 190, y: 620)
+        // 设置 UISwitch 开启状态的颜色
+        si.onTintColor = UIColor.green
+        // 设置 UISwitch 普通状态的边框颜色
+        si.tintColor = UIColor.red
+        // 设置 UISwitch 滑块的颜色
+        si.thumbTintColor = UIColor.white
+        // 设置 UISwitch 初始状态
+        si.isOn = true
+        // 设置 UISwitch 开关样式
+        si.preferredStyle = UISwitch.Style.checkbox
+        self.view.addSubview(si)
+    }
+    
+    func addUIPageControlView() {
+        // 创建分页控制器
+        let pageControl = UIPageControl(frame: CGRect(x: 25, y: 650, width: 280, height: 30))
+        // 设置 UIPageControl 页数
+        pageControl.numberOfPages = 6
+        // 设置 UIPageControl 背景色
+        pageControl.backgroundColor = UIColor.gray
+        // 设置 UIPageControl 页码点背景色
+        pageControl.pageIndicatorTintColor = UIColor.white
+        // 设置 UIPageControl 选中的页码点背景色
+        pageControl.currentPageIndicatorTintColor = UIColor.red
+        // 设置 UIPageControl 选中的页码数
+        pageControl.currentPage = 2
+        
+        pageControl.addTarget(self, action: #selector(pageChange), for: UIControl.Event.valueChanged)
+        
+        self.view.addSubview(pageControl)
+    }
+    
+    @objc func pageChange(pc: UIPageControl) {
+        print("分页控制器当前页码：\(pc.currentPage)")
+        if (clearAll) {
+            addUISegmentedControlView()
+            insertCart = false
+            insertMine = false
+            insertClear = false
+            clearAll = false
+            updateImage = false
+            updateTitle = false
+        }
+    }
+    
+    func addUISegmentedControlView() {
+        // 创建分段控制器
+//        let segmentedControl = UISegmentedControl(items: ["Home", "Circle", "Cart", "Mine"])
+        let segmentedControl = UISegmentedControl(items: ["Home", "Circle"])
+
+        // 设置 UISegmentedControl 控件位置
+        segmentedControl.frame = CGRect(x: 25, y: 700, width: 340, height: 38)
+        // 设置 UISegmentedControl 风格颜色
+        segmentedControl.tintColor = UIColor.gray
+        // 监听 UISegmentedControl 事件
+        segmentedControl.addTarget(self, action: #selector(selectChange), for: UIControl.Event.valueChanged)
+        
+        self.view.addSubview(segmentedControl)
+    }
+    
+    var insertCart = false
+    var insertMine = false
+    var insertClear = false
+    var clearAll = false
+    
+    var updateImage = false
+    var updateTitle = false
+    
+    @objc func selectChange(sc: UISegmentedControl) {
+        // 选中状态会消失
+//        sc.isMomentary = true
+        print("分段控制器当前页码：\(sc.selectedSegmentIndex)")
+        if (sc.selectedSegmentIndex == 1 && !insertCart) {
+            insertCart = true
+            // UISegmentedControl 动态插入文字 item
+            sc.insertSegment(withTitle: "Cart", at: 2, animated: true)
+        } else if (sc.selectedSegmentIndex == 2 && !insertMine) {
+            insertMine = true
+            // UISegmentedControl 动态插入图片 item
+            sc.insertSegment(with: UIImage(named: "portrait")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal), at: 3, animated: false)
+        } else if (sc.selectedSegmentIndex == 3) {
+            if (clearAll) {
+                // UISegmentedControl 删除所有 item
+                sc.removeAllSegments()
+                return
+            }
+            if (!insertClear) {
+                insertClear = true
+                // UISegmentedControl 动态插入图片 item
+                sc.insertSegment(with: UIImage(named: "clear")?.withRenderingMode(UIImage.RenderingMode.automatic), at: 4, animated: true)
+                // 设置 UISegmentedControl item 宽度 40
+                sc.setWidth(40, forSegmentAt: 4)
+            }
+        } else if (insertCart && insertMine && insertClear && !updateImage) {
+            // UISegmentedControl 修改指定位置的图片
+            sc.setImage(UIImage(named: "EyeInvisible")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal), forSegmentAt: 3)
+            updateImage = true
+        } else if (insertCart && insertMine && insertClear && !updateTitle) {
+            // UISegmentedControl 修改指定位置的文本
+            sc.setTitle("Recommend", forSegmentAt: 1)
+            // 设置 UISegmentedControl 自适应内容的宽度
+            sc.apportionsSegmentWidthsByContent = true
+            updateTitle = true
+        } else if (sc.selectedSegmentIndex == 4) {
+            // UISegmentedControl 删除指定 item
+            sc.removeSegment(at: 3, animated: true)
+            clearAll = true
+        }
+        
+    }
     
 }
 
