3e34da310cb18d9b7197263021af069a8b375430
William <yangchen_0808@163.com>
2022-01-06 22:00:58 +0800

    update table view demo

--- iOSAppDemo/TableViewController.swift
+++ iOSAppDemo/TableViewController.swift
@@ -15,6 +15,7 @@ class TableViewController: UITableViewController {
     
     var heightForHeader = 35
     var heightForFooter = 25
+    var heightForCell = 45
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -108,6 +109,62 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         return CGFloat(heightForFooter)
     }
+    
+    // 当 Cell 将要展示出来时被调用
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+    }
+    
+    // 当头部视图将要展示出来时被调用
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        
+    }
+    
+    // 当尾部视图将要展示出来时被调用
+    override func tableView(_ tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
+        
+    }
+    
+    // 当 Cell 已经被展示出来时调用
+    override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+    }
+    
+    // 当头部视图已经被展示出来时调用
+    override func tableView(_ tableView: UITableView, didEndDisplayingHeaderView view: UIView, forSection section: Int) {
+        
+    }
+    
+    // 当尾部视图已经被展示出来时调用
+    override func tableView(_ tableView: UITableView, didEndDisplayingFooterView view: UIView, forSection section: Int) {
+        
+    }
+    
+    // 设置 Cell 高度
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return CGFloat(heightForCell)
+    }
+    
+    // 用户将要选中某一行时调用
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return indexPath
+    }
+    
+    // 用户将要取消选中某一行时调用
+    override func tableView(_ tableView: UITableView, willDeselectRowAt indexPath: IndexPath) -> IndexPath? {
+        return indexPath
+    }
+    
+    // 用户已经选中某一行时调用
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    // 用户已经取消选中某一行时调用
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        
+    }
+    
 
     /*
     // Override to support conditional editing of the table view.
